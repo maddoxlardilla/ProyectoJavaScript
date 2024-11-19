81811f85d3525de211b51e3d4692d5acff10dae7
flores2006 <andresjf260@gmail.com>
2024-11-19 14:22:01 -0400

    Funciones

---
+++ JS/main.js
@@ -0,0 +1,43 @@
+
+function Sumar(){
+    let nUno=6;
+    let nDos=6;
+    let nTres=8;
+    let resultadoSuma=nUno+nDos+nTres;
+
+    alert("la suma es: "+resultadoSuma);
+}
+
+
+
+function Restar(){
+    let nUno=6;
+    let nDos=6;
+    let nTres=8;
+    let resultadoResta=nUno-nDos-nTres;
+
+    alert("la resta es: "+resultadoResta);
+}
+
+
+
+function Multiplicar(){
+    let nUno=6;
+    let nDos=6;
+    let nTres=8;
+    let resultadoMultiplicar=nUno*nDos*nTres;
+
+    alert("la multiplicacion es: "+resultadoMultiplicar);
+}
+
+
+
+function Dividir(){
+    let nUno=6;
+    let nDos=6;
+    let nTres=8;
+    let resultadoDividir=nUno/nDos/nTres;
+
+    alert("la division es: "+resultadoDividir);
+}
+
